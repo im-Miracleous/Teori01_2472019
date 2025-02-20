2cd1e00a8b7f7739a2b46f02ca245bfe4ccfeb1e
Miracle Steven <miraclesteven05@gmail.com>
2025-02-20 22:13:17 +0700

    Edit komentar dan kode pada tugas Teori01B

--- Teori01A_2472019.py
+++ Teori01A_2472019.py
@@ -2,7 +2,10 @@
 # Penulis: Miracle Steven Gerrald
 # Tujuan program: Membaca suatu bilangan integer yang menyatakan jumlah minggu, dan mencetak berapa tahun dan bulan serta minggu yang setara dengan jumlah hari yang diinputkan.
 # ---- Kamus Data ----
-# i : variabel integer
+# input_awal : jumlah minggu yang ingin dikonversi oleh pengguna
+# year : jumlah tahun dari pembagian input_awal
+# month : jumlah bulan dari pembagian input_awal
+# week : jumlah minggu dari sisa pembagian input_awal
 
 def main():
     # INPUT
--- Teori01B_2472019.py
+++ Teori01B_2472019.py
@@ -2,25 +2,29 @@
 # Penulis: Miracle Steven Gerrald
 # Tujuan program: Membaca variabel nama, tinggi, dan tahun sesuai dengan nilai yang diinput user, dan mencetak tinggi badan anak tersebut beberapa tahun kemudian. Asumsi anak sehat, setiap tahun tingginya bertambah 5 cm.
 # ---- Kamus Data ----
-# i : variabel integer
+# today = tanggal hari ini (menggunakan bentuk datetime.date.today() yang diimport dari modul datetime, agar hasil tanggal yang ditampilkan adaptif)
+# tahun_awal = tahun ini (menggunakan today.year untuk menampilkan hasil tahun yang dinamis)
+# nama = nama anak
+# tinggi = tinggi anak
+# tahun_akhir = tahun yang diinput user
 
 import datetime
 
 def main():
     # INPUT / STORED DATA / PREP
     today = datetime.date.today()
-    current_year = today.year
+    tahun_awal = today.year
 
-    nama = input("Nama  : ")
+    nama = input("Nama        : ")
     tinggi = int(input("Tinggi (cm) : "))
-    tahun = int(input("Tahun    : "))
+    tahun_akhir = int(input("Tahun       : "))
 
     # OUTPUT
     print(f'''
     ====================================
     Sekarang tinggi {nama} {tinggi} cm
     ------------------------------------
-    Tahun {tahun + 5}, tingginya menjadi {tinggi + 5}
+    Tahun {tahun_akhir}, tingginya menjadi {tinggi + 5 * (tahun_akhir - tahun_awal)} cm
     ====================================
           ''')
     
